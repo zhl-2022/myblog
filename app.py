6f62c9c2d27012b0932f8c919e77aad1ec3308e9
zhl <zhanghuiliu2020@163.com>
2024-07-13 22:51:52 +0800

    add versel.json 测试

--- app.py
+++ app.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/myblog-ochre-nu.vercel.app/')
+@app.route('/')
 def home():  # put application's code here
     return render_template('index.html')
 
 
 
-@app.route('/myblog-ochre-nu.vercel.app/blog')
+@app.route('/blog')
 def blog():  # put application's code here
     return render_template('blog.html')
 
 
-@app.route('/myblog-ochre-nu.vercel.app/single_blog')
+@app.route('/single_blog')
 def single_blog():  # put application's code here
     return render_template('single_blog2.html')
 
 
-@app.route('/myblog-ochre-nu.vercel.app/contact')
+@app.route('/contact')
 def contact():  # put application's code here
     return render_template('contact.html')
 
-@app.route('/myblog-ochre-nu.vercel.app/pdf')
+@app.route('/pdf')
 def pdf():  # put application's code here
     return render_template('contact.html')
 
