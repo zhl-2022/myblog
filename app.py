495b20f11ad5ff474b0bd4b04b72d6004cd58061
zhl <zhanghuiliu2020@163.com>
2024-07-13 21:37:15 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def home():  # put application's code here
+    return render_template('index.html')
+
+
+
+@app.route('/blog')
+def blog():  # put application's code here
+    return render_template('blog.html')
+
+
+@app.route('/single_blog')
+def single_blog():  # put application's code here
+    return render_template('single_blog2.html')
+
+
+@app.route('/contact')
+def contact():  # put application's code here
+    return render_template('contact.html')
+
+@app.route('/pdf')
+def pdf():  # put application's code here
+    return render_template('contact.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+File:       config.py
+Time:       2024-07-12-12:58
+User:       zhl
+"""
+
+
+# config.py
+class Config:
+    TEMPLATES_AUTO_RELOAD = True
+
+
+
