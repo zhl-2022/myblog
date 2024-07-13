f4ba66787b80ccd364ab339a246b15cb6a75031f
zhl <zhanghuiliu2020@163.com>
2024-07-13 22:40:02 +0800

    change versel 域名测试

--- app.py
+++ app.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route('/myblog-ochre-nu.vercel.app/')
 def home():  # put application's code here
     return render_template('index.html')
 
 
 
-@app.route('/blog')
+@app.route('/myblog-ochre-nu.vercel.app/blog')
 def blog():  # put application's code here
     return render_template('blog.html')
 
 
-@app.route('/single_blog')
+@app.route('/myblog-ochre-nu.vercel.app/single_blog')
 def single_blog():  # put application's code here
     return render_template('single_blog2.html')
 
 
-@app.route('/contact')
+@app.route('/myblog-ochre-nu.vercel.app/contact')
 def contact():  # put application's code here
     return render_template('contact.html')
 
-@app.route('/pdf')
+@app.route('/myblog-ochre-nu.vercel.app/pdf')
 def pdf():  # put application's code here
     return render_template('contact.html')
 
