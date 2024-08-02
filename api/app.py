29429e11bef01959b9b519fb34eab3386b647947
zhl <zhanghuiliu2020@163.com>
2024-08-02 20:10:39 +0800

    fix version 0.1

--- api/app.py
+++ api/app.py
@@ -24,17 +24,17 @@ usersTable()
 postsTable()
 commentsTable()
 
-match __name__:
-    case "__main__":
-        startTime = currentTimeStamp()
-        Log.app(f"Running on http://{APP_HOST}:{APP_PORT}")
-        Log.success("App started")
-        app.run(debug=DEBUG_MODE, host=APP_HOST, port=APP_PORT)
-        endTime = currentTimeStamp()
-        runTime = endTime - startTime
-        runTime = str(timedelta(seconds=runTime))
-        Log.app(f"Run time: {runTime} ")
-        Log.app("Shut down")
-        Log.warning("App shut down")
-        Log.breaker()
+# match __name__:
+#     case "__main__":
+startTime = currentTimeStamp()
+Log.app(f"Running on http://{APP_HOST}:{APP_PORT}")
+Log.success("App started")
+app.run(debug=DEBUG_MODE, host=APP_HOST, port=APP_PORT)
+endTime = currentTimeStamp()
+runTime = endTime - startTime
+runTime = str(timedelta(seconds=runTime))
+Log.app(f"Run time: {runTime} ")
+Log.app("Shut down")
+Log.warning("App shut down")
+Log.breaker()
 
