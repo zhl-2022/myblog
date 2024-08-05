bc5fee2f538574bd07357b0b7536235cbed604cd
zhl-2022 <104522719+zhl-2022@users.noreply.github.com>
2024-08-05 11:00:26 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,7 +2,6 @@ import sys
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-# 将项目根目录添加到系统路径
 sys.path.append(os.path.join(current_dir, '..'))
 
 from factory import create_app
@@ -10,4 +9,4 @@ from factory import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
